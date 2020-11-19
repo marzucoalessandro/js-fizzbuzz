274768eca74279b983cc400a6eb07e7c34b4704d
Alessandro <marzucoalessandro1@gmail.com>
2020-11-19 17:26:02 +0100

    ok gira tutto.

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,10 +29,10 @@ for ( i=1; i<101; i++) {
   //abbiamo verificato che il nostro ciclo ha effettivamente contato tutti i numeri
   //impostiamo le ns condizioni
 
-  if (i %3 == 0) and (i%5 == 0) {
+  if (i %3 == 0 && i%5 == 0) {
     console.log("fizzbuzz");
   } else if (i %3 == 0) {
-      console.log("fizz");
+    console.log("fizz");
   } else if (i %5 == 0) {
     console.log("buzz");
   } else {

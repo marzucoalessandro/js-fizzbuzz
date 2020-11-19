a685b9c8346c15493adeda7a94d48ea3f8aef363
Alessandro <marzucoalessandro1@gmail.com>
2020-11-19 15:02:24 +0100

    ciclo for per  contatore da 1 a 100

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,4 +4,28 @@
 //Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 //Prima di partire a scrivere codice poniamoci qualche domanda:
 //Come faccio a sapere se un numero è divisibile per?
-//Abbiamo visto qualcosa di particolare che possiamo usare?
+//abbiamo visto qlk di particolare che possiamo usare?
+
+//1 creaimo un array vuoto.
+
+//2 impostiamo un ciclo for per far si che conti tutti i numeri da 0 a 100;
+
+//3 a questo punto inseriamo i numeri generati dal programma nell array
+
+//4 impostiamo le ns condizioni:
+
+// se il numero è multiplo di 3 allora stampi Fizz
+
+// se il numero è multiplo di 5 stampi Buzz
+
+// ulteriore condizione se il numero è multiplo di 3 e 5 stampi fizzbuzz.
+ var contenitore = [];
+ console.log(contenitore);
+ //il ns array vuoto visibile in console.
+ var startnumber = Number(0);
+
+ for ( i=1; i<101; i++) {
+   console.log(startnumber = 0 + i);
+   //abbiamo verificato che il nostro ciclo ha effettivamente contato tuti i numeri
+   
+ }
